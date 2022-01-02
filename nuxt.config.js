5ea1df749d1eb2052435df08fd968e5e26ae83f5
Kajsa1 <kajsaKeklof@gmail.com>
2022-01-02 17:55:25 +0000

    updated config and removed cname file

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
   router: {
-    base: "/portfolio/",
+    base: "/kajsaeklof.github.io/",
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
