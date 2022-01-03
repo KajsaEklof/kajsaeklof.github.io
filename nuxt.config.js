190cdffee2284a203a1a21832f1c87f8c7a15830
Kajsa1 <kajsaKeklof@gmail.com>
2022-01-03 12:35:51 +0000

    removed gh+pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,6 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
-  router: {
-    base: "/kajsaeklof.github.io/",
-  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
