497dc846ca78988cab8f747a4ee8edffd5f0ffbf
Kajsa Eklof <kajsakeklof@gmail.com>
2024-06-11 16:13:31 +0100

    fix: mobile menu not loading

--- app.vue
+++ app.vue
@@ -2,19 +2,32 @@
 import { useMediaQuery } from '@vueuse/core';
 
 const isLargeScreen = useMediaQuery('(min-width: 1024px)');
+const showMobileMenu = ref(false);
 
 watch(isLargeScreen, (isLarge) => {
+  if (isLarge) {
+    showMobileMenu.value = false;
+  } else {
+    showMobileMenu.value = true;
+  }
+  
   const body = document.querySelector('body');
 
   body?.classList.remove('hide-overflow');
+});
+
+onMounted(() => {
+  if (isLargeScreen.value) {
+    showMobileMenu.value = true;
+  }
 });
 </script>
 
 <template>
   <div class="layout-wrapper">
-    <TheNavigation v-if="isLargeScreen" />
-    <TheMobileNavigation v-else />
-    <main :class="['content', { mobile: !isLargeScreen }]">
+    <TheMobileNavigation v-if="showMobileMenu" />
+    <TheNavigation v-else />
+    <main :class="['content', { mobile: !showMobileMenu }]">
       <NuxtPage />
     </main>
     <TheFooter :class="{ mobile: !isLargeScreen }" />
